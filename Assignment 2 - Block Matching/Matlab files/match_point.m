c8abd05395dd7e59a95579ef1b9d719cbb5b0a3b
LeisAlAyoubi <leisayoubi@web.de>
2021-08-12 19:57:52 +0200

    Completed Ex2.1

---
+++ Assignment 2 - Block Matching/Matlab files/match_point.m
@@ -0,0 +1,7 @@
+function [best_point] = match_point(I1,I2,search_area, window_size)
+%MATCH_POINT Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
